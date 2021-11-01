ad39816de72ccc9f205ccc936882f22d2e1c04c3
Alisa0908 <alisa0908piano@gmail.com>
2021-11-01 16:20:06 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Syoryuken
+//
+//  Created by 松尾有紗 on 2021/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var charaImage: UIImageView!
+    
+    var imageArrayAttack : Array<UIImage> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        while let attackImage = UIImage(named: "attak\(imageArrayAttack.count+1)") {
+                    imageArrayAttack.append(attackImage)
+                }
+    }
+
+    @IBAction func attackBtn(_ sender: Any) {
+        // アニメーションの適用
+        charaImage.animationImages = imageArrayAttack
+        // 1.5秒間隔
+        charaImage.animationDuration = 1
+        // 1回繰り返し
+        charaImage.animationRepeatCount = 1
+        // アニメーションを開始
+        charaImage.startAnimating()
+    }
+    
+}
+
